bfaac7d332d2664a48d2fed3f327a21e2a82b9da
JaseFreemanBirdsnest <jason.freeman@birdsnest.com.au>
2023-01-17 15:06:56 +1100

    Completed exercise 2, all tests passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,20 @@
-const repeatString = function() {
+let str = "hey";
+let num = 3;
+
+const repeatString = function(str, num) {
+    let i = 0;
+    let rtnStr = [];
+
+    if (num < 0){
+        return "ERROR";
+    }
+
+    while (i < num){
+        rtnStr.push(str);
+        i++;
+    }
+    
+    return rtnStr.join("");
 
 };
 
