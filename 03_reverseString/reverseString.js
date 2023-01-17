3bc36d35423c51071f4abf0e830ebc2615e6b08a
JaseFreemanBirdsnest <jason.freeman@birdsnest.com.au>
2023-01-18 06:01:33 +1100

    Completed task three, only small interations needed on first try

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+let userString //= prompt("Please enter a single word or a phrase, I'll reverse it");
 
+const reverseString = function(userString) {
+    
+    let strArray = userString.split("");
+
+    strArray.reverse();
+    
+    return strArray.join('');
 };
 
 // Do not edit below this line
