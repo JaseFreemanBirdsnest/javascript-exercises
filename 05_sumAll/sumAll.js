45aa8bd7a3300bdf36c0bc1658d219a7ec2ca69f
JaseFreemanBirdsnest <jason.freeman@birdsnest.com.au>
2023-01-19 14:40:21 +1100

    completed exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,33 @@
-const sumAll = function() {
+const sumAll = function(firstNum, lastNum) {
+
+    if (firstNum > lastNum){
+        let a = firstNum;
+        let b = lastNum;
+        firstNum = b;
+        lastNum = a;
+    }
+
+    if (firstNum < 0 || lastNum < 0){
+        return "ERROR";
+    }
+
+    
+    if (typeof firstNum !== "number" || typeof lastNum !== "number" ){
+        return "ERROR";
+    }
+
+    const numArray = [];
+    let i = firstNum;
+
+    while (i <= lastNum){
+        numArray.push(i);
+        i++;
+    }
+
+   return  total = numArray.reduce((acc, cV) => acc + cV, 0);
 
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
