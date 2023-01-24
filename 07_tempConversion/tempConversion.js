c40c0311396dfb3aec287eea75214a88b65d706e
JaseFreemanBirdsnest <jason.freeman@birdsnest.com.au>
2023-01-24 12:53:43 +1100

    Completed exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,23 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+
+  if (tempF == 32){
+    return 0;
+  }
+  else
+
+  return convertedTemp = Math.round((tempF - 32) * (5/9)*10) / 10;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+
+  if(tempC == 0){
+    return 32;
+  }
+  else
+  
+  return convertedTemp = Math.round((tempC * 9/5 + 32)*10)/10;
+
 };
 
 // Do not edit below this line
