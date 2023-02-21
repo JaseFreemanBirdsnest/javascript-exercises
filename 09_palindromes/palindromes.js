616bd467bcc1a7aa933f11fca7a8db4591fd9df9
JaseFreemanBirdsnest <jason.freeman@birdsnest.com.au>
2023-02-21 15:35:17 +1100

    function to remove spaces and punctuation, and to lowercase.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (phrase) {
+    
+    
 };
 
+function preparation(phrase){
+    noPunct = phrase.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    noSpaces = noPunct.replaceAll(' ','');
+    prepdPhrase = noSpaces.toLowerCase();
+
+}
+ 
 // Do not edit below this line
 module.exports = palindromes;
