8a1ed5387ab72dbcc8d7fbbacd7b8b0f4e752d45
JaseFreemanBirdsnest <jason.freeman@birdsnest.com.au>
2023-02-21 14:58:44 +1100

    completed the exercise, this one wasnt too difficult.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,28 @@
-const add = function() {
-	
+const add = function(a,b){
+  return result = a + b;
+}
+
+const subtract = function(a, b) {
+	return result = a - b;
 };
 
-const subtract = function() {
-	
+const sum = function(ary) {
+  return result = ary.reduce((tot, num) => tot + num, 0);
 };
 
-const sum = function() {
-	
+const multiply = function(ary) {
+  return result = ary.reduce((tot, num) => tot * num);
 };
 
-const multiply = function() {
-
+const power = function(a,b) {
+	return result = a ** b;
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function fact(x) {
+	if (x == 0 || x == 1){
+    return 1;
+  }
+  return fact(x-1)*x;
 };
 
 // Do not edit below this line
