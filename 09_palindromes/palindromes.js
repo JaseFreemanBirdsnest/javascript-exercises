f6e9eea951f0199db904ba57de956e88ff2f1075
JaseFreemanBirdsnest <jason.freeman@birdsnest.com.au>
2023-02-23 12:23:22 +1100

    was almost there with my logic, but needed a little help, checked the solutions as I was sure that i wasnt far off

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,14 +1,29 @@
 const palindromes = function (phrase) {
     
+    const preppedString = phrase.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+
+    return (
+        preppedString
+            .split("")
+            .reverse()
+            .join("") == preppedString
+    );
+
+    // preparation(phrase);
+
+    // let phraseAry = Array.from(prepdPhrase);
+    // let revAry = phraseAry.reverse();
+    // let phraseRev = revAry.toString();
     
-};
+    // return (prepdPhrase == phraseRev) ? true:false;
+};  
 
-function preparation(phrase){
-    noPunct = phrase.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
-    noSpaces = noPunct.replaceAll(' ','');
-    return prepdPhrase = noSpaces.toLowerCase();
+// function preparation(phrase){
+//     noPunct = phrase.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+//     noSpaces = noPunct.replaceAll(' ','');
+//     return prepdPhrase = noSpaces.toLowerCase();
 
-}
- 
+// }
+
 // Do not edit below this line
 module.exports = palindromes;
